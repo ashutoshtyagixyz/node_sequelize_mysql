b30e59c0d5a2410d4d8301959c359c3150391eb9
28ayushsharma <28ayushsharma@gmail.com>
2020-01-07 00:15:14 +0530

    sequelize setup

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,10 +1,9 @@
 const Sequelize = require("sequelize");
 
-const sequelize = new Sequelize("learning_n","root","123456",
+const sequelize = new Sequelize("learning_n","dev","123456",
 					{
 						host : '127.0.0.1',
-						dialect : "mysql",
-						operatorsAliases : false 	
+						dialect : "mysql" 	
 					});
 
 module.exports = sequelize;
