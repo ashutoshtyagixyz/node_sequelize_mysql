64aa340254ca7f92260235602155427ff53006a4
28ayushsharma <28ayushsharma@gmail.com>
2020-01-10 08:53:11 +0530

    sequalize and nodemon installed

--- app.js
+++ app.js
@@ -1,11 +1,14 @@
 const http = require('http');
 var express = require('express');
+var logger = require('morgan');
 var appRoutes = require('./routes/app.route');
 
 //db connection
 require('./src/database/connection');
 
 var app = express();
+
+//app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
