3a0f8fbf758666ebe07596786d17251c8a88af25
28ayushsharma <28ayushsharma@gmail.com>
2020-01-11 23:38:33 +0530

    basic

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
+const path = require('path');
 const http = require('http');
-var express = require('express');
-var logger = require('morgan');
+const express = require('express');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+
 var appRoutes = require('./routes/app.route');
 
 //db connection
@@ -9,11 +12,15 @@ require('./src/database/connection');
 var app = express();
 
 //app.use(logger('dev'));
+app.use(bodyParser.urlencoded( { extended:false } ));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', appRoutes);
 
-//app.use(express.static(path.join(__dirname, 'public')));
+app.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+});
+
 
 module.exports = app;
--- routes/app.route.js
+++ routes/app.route.js
@@ -1,11 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const path = require('path');
+const express = require('express');
+const router = express.Router();
+
+const rootDir = require('../util/path');
+
 
 router.get('/', function (req, res, next) {
     res.status(200).json({
     	"status"  : true,
     	"message" : "request received."
     })  
+});
+
+router.get('/home', function(req,res,next){
+    res.sendFile(path.join(rootDir ,'views','home.html'))
 });
 
 module.exports = router;
